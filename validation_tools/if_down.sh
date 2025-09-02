325184fe9a47e9154d0163376c7832fcfd27e835
Tim Gebauer <tim.gebauer@tu-dortmund.de>
2025-09-02 10:11:10 +0200

    Remove old validation files

--- validation_tools/if_down.sh
+++
@@ -1,2 +0,0 @@
-sudo tc qdisc replace dev tun0 root noqueue
-sudo ip link delete tun0
--- validation_tools/if_up_multi_queue.sh
+++
@@ -1,3 +0,0 @@
-sudo ip tuntap add dev tun0 mode tun multi_queue
-sudo ip addr add 10.0.3.0/24 dev tun0
-sudo ip link set dev tun0 up
